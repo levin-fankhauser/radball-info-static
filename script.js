5748f689d321f803ce9c3c49b2ebf08dbbc545a4
levin-fankhauser <levin.fankhauser@gmx.ch>
2023-06-27 09:17:27 +0200

    Small changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const circle = document.getElementById('circle');
-const circlestyle = circle.style;
+const circles = document.querySelectorAll('.circle');
 
-document.addEventListener('mousemove', e => {
-    window.requestAnimationFrame(() => {
-        circlestyle.top = `${e.clientY - circle.offsetHeight / 2}px`;
-        circlestyle.left = `${e.clientX - circle.offsetWidth / 2}px`;
-    })
+circles.forEach(circle => {
+    const circleStyle = circle.style;
+    document.addEventListener('mousemove', e => {
+        window.requestAnimationFrame(() => {
+            circleStyle.top = `${e.clientY - circle.offsetHeight / 2}px`;
+            circleStyle.left = `${e.clientX - circle.offsetWidth / 2}px`;
+        });
+    });
 });
-
-//Script erstellt mit YouTube Tutorial
